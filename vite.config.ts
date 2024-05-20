26c9f652b7572452746db9189643b0982d27349f
atippaz <nasakun13201@gmail.com>
2024-05-20 22:07:01 +0700

    fix conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const isDevelopment = mode === 'development'
+  // const isDevelopment = mode === 'development'
   return {
     plugins: [vue()],
-    build: {
-      // the built files will be added here
-      outDir: './public/app',
-    },
-    base: isDevelopment ? '/' : '/app/',
+
+    // base: isDevelopment ? '/' : '/app/',
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
