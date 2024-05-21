58a8d958604fff2467bd2e180a7ae417bcdf52db
atippaz <nasakun13201@gmail.com>
2024-05-21 08:28:41 +0700

    add pwa

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from '@/router'
 import VueApexCharts from 'vue3-apexcharts'
-
+import './registerServiceWorker'
 const app = createApp(App)
 app.use(router).component('apexchart', VueApexCharts)
 app.mount('#app')
