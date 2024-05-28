c607fcea73e4c47dbfb6d01c3ea874c4822e901d
atippaz <nasakun13201@gmail.com>
2024-05-28 20:57:09 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './docs',
+  },
   plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
